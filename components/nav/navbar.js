fcfa171560ea685e1f9fb69347e6b585cc319311
roluochKe <rasholuoch@gmail.com>
2022-01-11 10:56:45 +0300

    banner component

---
+++ components/nav/navbar.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './navbar.module.css'
+
+const NavBar = (props) => {
+  return (
+    <div className={styles.container}>
+      <h1>navbar</h1>
+    </div>
+  )
+}
+
+export default NavBar
