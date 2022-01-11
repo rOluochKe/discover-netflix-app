5fd201ae8c4d0ec89aa9db5ada6f2404964a4fb0
roluochKe <rasholuoch@gmail.com>
2022-01-11 21:36:31 +0300

    more component and authentication

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,43 @@ import styles from '../styles/Home.module.css'
 
 import Banner from '../components/banner/banner'
 import NavBar from '../components/nav/navbar'
+import SectionCards from '../components/card/section-cards'
 
-export default function Home() {
+import {
+  getVideos,
+  getPopularVideos,
+  getWatchItAgainVideos,
+} from '../lib/videos'
+import useRedirectUser from '../utils/redirectUser'
+
+export async function getServerSideProps(context) {
+  const { userId, token } = await useRedirectUser(context)
+  const watchItAgainVideos = await getWatchItAgainVideos(userId, token)
+
+  const disneyVideos = await getVideos('disney trailer')
+  const productivityVideos = await getVideos('Productivity')
+
+  const travelVideos = await getVideos('indie music')
+
+  const popularVideos = await getPopularVideos()
+  return {
+    props: {
+      disneyVideos,
+      travelVideos,
+      productivityVideos,
+      popularVideos,
+      watchItAgainVideos,
+    },
+  }
+}
+
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+  popularVideos,
+  watchItAgainVideos,
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -22,6 +57,22 @@ export default function Home() {
           subTitle='a very cute dog'
           imgUrl='/static/clifford.webp'
         />
+
+        <div className={styles.sectionWrapper}>
+          <SectionCards title='Disney' videos={disneyVideos} size='large' />
+          <SectionCards
+            title='Watch it again'
+            videos={watchItAgainVideos}
+            size='small'
+          />
+          <SectionCards title='Travel' videos={travelVideos} size='small' />
+          <SectionCards
+            title='Productivity'
+            videos={productivityVideos}
+            size='medium'
+          />
+          <SectionCards title='Popular' videos={popularVideos} size='small' />
+        </div>
       </div>
     </div>
   )
